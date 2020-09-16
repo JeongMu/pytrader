e06c305cef8fc99e0ce7c2946962d572b7381eb1
DESKTOP-T80EHS0\user <kang815595@gmail.com>
2020-09-16 10:16:43 +0900

    1. 모듈 내 함수 open_browser()을 크롤러클래스 내부로 옮김 2. 분기데이터도 DB에 저장하도록 변경

--- crawler.py
+++ crawler.py
@@ -5,50 +5,52 @@ import sqlite3
 driver = webdriver.Chrome("chromedriver.exe")
 
 
-def open_browser(url):
-    driver.get(url)
-    source = driver.page_source
-    table_list = pd.read_html(source)
-
-    return table_list
-
-
 class CrawlData:
-    def __init__(self, code):
-        pass
 
     def get_quarter_data(self):
         pass
 
     def get_year_data(self):
+        pass
+
+    @staticmethod
+    def open_browser():
         pass
 
 
 class CrawlFinanceStatement(CrawlData):
     def __init__(self, code):
         url = "https://navercomp.wisereport.co.kr/v2/company/c1010001.aspx?cmp_cd=%s&amp;target=finsum_more" % code
-        table_list = open_browser(url)
+        table_list = self.open_browser(url)
 
         self.finance_statement = table_list[12]
 
     def get_quarter_data(self):
-        y_statement = self.finance_statement['연간연간컨센서스보기']
+        y_statement = self.finance_statement['분기분기컨센서스보기']
         y_statement.index = self.finance_statement[self.finance_statement.columns[0]]
 
         return y_statement
 
     def get_year_data(self):
-        q_statement = self.finance_statement['분기분기컨센서스보기']
+        q_statement = self.finance_statement['연간연간컨센서스보기']
         q_statement.index = self.finance_statement[self.finance_statement.columns[0]]
 
         return q_statement
+
+    @staticmethod
+    def open_browser(url):
+        driver.get(url)
+        source = driver.page_source
+        table_list = pd.read_html(source)
+
+        return table_list
 
 
 class CrawlFinanceRatio(CrawlData):
     def __init__(self, code):
         url = "http://comp.fnguide.com/SVO2/asp/" \
               "SVD_FinanceRatio.asp?pGB=1&gicode=A%s&cID=30&MenuYn=Y&ReportGB=&NewMenuID=104&stkGb=701" % code
-        self.table_list = open_browser(url)
+        self.table_list = self.open_browser(url)
 
     def get_year_data(self):
         data = self.table_list[0]
@@ -62,16 +64,19 @@ class CrawlFinanceRatio(CrawlData):
         data.drop(data.columns[0], axis='columns', inplace=True)
         return data
 
+    @staticmethod
+    def open_browser(url):
+        driver.get(url)
 
-def crawl_data(code, mode):
-    if mode == 0:
-        Crawler = CrawlFinanceStatement(code)
-    elif mode == 1:
-        Crawler = CrawlFinanceRatio(code)
-    else:
-        return None
+        for index in range(1, 24):
+            driver.find_element_by_id('grid1_%d' % index).click()
+        for index in range(1, 8):
+            driver.find_element_by_id('grid2_%d' % index).click()
 
-    return Crawler.get_year_data()
+        source = driver.page_source
+        table_list = pd.read_html(source)
+
+        return table_list
 
 
 def get_code_list():
@@ -85,13 +90,19 @@ if __name__ == "__main__":
 
     # crawl statement
     con_statementDB = sqlite3.connect('data/shorted_finance_statement.db')
+    con_QstatementDB = sqlite3.connect('data/quarter_finance_statement.db')
     for i, v in enumerate(kospi_code_list):
         # noinspection PyBroadException
         try:
-            data = crawl_data(v, 0)
+            crawler = CrawlFinanceStatement(v)
+            data = crawler.get_year_data()
+            Qdata = crawler.get_quarter_data()
         except:
             print(i, '/', code_list_length, ' failed saving statement')
+            continue
+
         data.to_sql(v, con_statementDB, if_exists='replace')
+        Qdata.to_sql(v, con_QstatementDB, if_exists='replace')
 
         print(i, '/', code_list_length, ' completed saving statement')
 
@@ -99,14 +110,19 @@ if __name__ == "__main__":
 
     # crawl ratio
     con_ratioDB = sqlite3.connect('data/finance_ratio.db')
+    con_QratioDB = sqlite3.connect('data/quarter_finance_ratio.db')
     for i, v in enumerate(kospi_code_list):
         # noinspection PyBroadException
         try:
-            data = crawl_data(v, 1)
+            crawler = CrawlFinanceRatio(v)
+            data = crawler.get_year_data()
+            Qdata = crawler.get_quarter_data()
         except:
             print(i, '/', code_list_length, ' failed saving ratio')
+            continue
 
         data.to_sql(v, con_ratioDB, if_exists='replace')
+        Qdata.to_sql(v, con_QratioDB, if_exists='replace')
 
         print(i, '/', code_list_length, ' completed saving ratio')
 
