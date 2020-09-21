2bef44cfb5095d05ebc8afe6d00326e525e5b077
DESKTOP-T80EHS0\user <kang815595@gmail.com>
2020-09-21 21:11:13 +0900

    매수대상 알고리즘 수정 매도대상 알고리즘 제작

--- pymon.py
+++ pymon.py
@@ -1,13 +1,8 @@
 import sys
 from PyQt5.QtWidgets import *
-import Kiwoom
-import time
 import pandas as pd
-import datetime
 import sqlite3
-
-MARKET_KOSPI = 0
-MARKET_KOSDAQ = 10
+import score_checker
 
 
 class PyMon:
@@ -15,46 +10,25 @@ class PyMon:
         self.kospi_codes = None
         self.kosdaq_codes = None
 
-        """
-        self.kiwoom = Kiwoom.Kiwoom()
-        self.kiwoom.comm_connect()
-        self.get_code_list()
-        """
-
         self.con_statementDB = sqlite3.connect('data/shorted_finance_statement.db')
         self.con_ratioDB = sqlite3.connect('data/finance_ratio.db')
 
         self.score_data = pd.read_excel('data/score.xlsx')
         self.buy_list_dataset = None
-        self.buy_list = None
+        self.buy_list = []
+        self.sell_list = []
 
+        f = open('data/portfolio.txt', 'r', encoding="UTF-8")
+        self.portfolio = f.readline().split()
 
-    def get_code_list(self):
-        self.kospi_codes = self.kiwoom.get_code_list_by_market(MARKET_KOSPI)
-        self.kosdaq_codes = self.kiwoom.get_code_list_by_market(MARKET_KOSDAQ)
-
-        f = open('data/kospi_code_list.txt', 'w', encoding="UTF-8")
-        for i in self.kospi_codes:
-            f.write(i + ' ')
-        f.close()
-
-        f = open('data/kosdaq_code_list.txt', 'w', encoding="UTF-8")
-        for i in self.kosdaq_codes:
-            f.write(i + ' ')
-        f.close()
+        self.set_buy_list_by_svm()
+        self.set_sell_list_by_svm()
 
     def check_f_score(self, score_data, target_score):
         for i in score_data.loc:
             return None
 
-    # 매수 리스트 업데이트
-    def update_buy_list(self, buy_list):
-        f = open("data/buy_list.txt", "wt", encoding="UTF-8")
-        for code in buy_list:
-            f.writelines("매수;" + code + ";시장가;10;0;매수전\n")
-        f.close()
-
-    def get_buy_list_super_value_momentum(self):
+    def set_buy_list_by_svm(self):
         dataset = self.get_clean_data()
 
         get_total_score = lambda data_list: data_list[0] - data_list[1] - data_list[2] - data_list[3]
@@ -65,18 +39,49 @@ class PyMon:
         dataset['total_score'] = col_total_score
         dataset = dataset.sort_values(by=['total_score'], axis=0, ascending=False)
 
-        print(dataset)
-
         self.buy_list_dataset = dataset[:50]
-        self.buy_list = self.buy_list_dataset['code']
 
-    def get_sell_list_super_value_momentum(self):
-        sell_list = self.buy_list_dataset[self.buy_list_dataset['1년 수익률'] < 0]
+        fixed_buy_list = self.buy_list_dataset[self.buy_list_dataset['1년 수익률'] > 0]
 
-        if len(sell_list) / len(self.buy_list_dataset) >= 0.6:
-            return self.buy_list_dataset
-        else:
-            return sell_list
+        if len(fixed_buy_list) / len(self.buy_list_dataset) <= 0.25:
+            temp_dict = {'code': [], '시장구분': [], 'f-score': [], '시가총액': [], 'GP/A': [], 'PBR': [], 'PER': [],
+                         '현재가': [], 'PSR': [], '1년 수익률': []}
+
+            fixed_buy_list = pd.DataFrame(temp_dict)
+
+        buy_list = list(fixed_buy_list['code'])
+
+        for code in buy_list:
+            if code in self.portfolio:
+                continue
+            else:
+                self.portfolio.append(code)
+                self.buy_list.append(code)
+
+    def set_sell_list_by_svm(self):
+        for code in self.portfolio:
+            if not(code in self.buy_list):
+                self.portfolio.remove(code)
+                self.sell_list.append(code)
+
+    # 매수 리스트 업데이트
+    def update_buy_list(self):
+        f = open("data/buy_list.txt", "wt", encoding="UTF-8")
+        for code in self.buy_list:
+            f.writelines("매수;" + code + ";시장가;10;0;매수전\n")
+        f.close()
+
+    def update_sell_list(self):
+        f = open("data/sell_list.txt", "wt", encoding="UTF-8")
+        for code in self.sell_list:
+            f.writelines("매도;" + code + ";시장가;10;0;매도전\n")
+        f.close()
+
+    def update_portfolio(self):
+        f = open("data/portfolio.txt", "wt", encoding="UTF-8")
+        for code in self.portfolio:
+            f.write(code + ' ')
+        f.close()
 
     @staticmethod
     def normalization(column):
@@ -105,37 +110,16 @@ class PyMon:
 
         return dataset
 
-    def run(self, f_target):
-        buy_list = []
-        # print(self.kospi_codes)
-        self.get_buy_list_super_value_momentum()
-        self.get_sell_list_super_value_momentum()
+    def run(self):
+        checker = score_checker.Checker()
+        checker.run()
 
-        """for i, code in enumerate(self.kospi_codes):
-
-            f = open('data/result_data.txt', 'w', encoding="UTF-8")
-            f.write(str(i) + '\n')
-
-            for j in buy_list:
-                f.write(j + ' ')
-            f.close()
-
-            # noinspection PyBroadException
-            try:
-                f_score = self.check_f_score(code)
-            except:
-                print("exception occured")
-                continue
-
-            print(i, '/', '1564 f_score = %d' % f_score)
-
-            if f_score >= f_target:
-                buy_list.append(code)"""
-
-        self.update_buy_list(buy_list)
+        self.update_buy_list()
+        self.update_sell_list()
+        self.update_portfolio()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     pymon = PyMon()
-    pymon.run(8)
+    pymon.run()
